4252b11d1764efbc85abc29e80140299cb4638d5
HugoNCastro <hugonaaman@gmail.com>
2023-09-26 16:56:38 -0300

    Create abstraction of entities
    Mapping entities props

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,8 +1,15 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface InstructorProps {
   name: string
 }
 
 export class Insturctor extends Entity<InstructorProps> {
+  static create(props: InstructorProps, id?: UniqueEntityID) {
+    const instructor = new Insturctor(props, id)
+
+    return instructor
+  }
+
 }
