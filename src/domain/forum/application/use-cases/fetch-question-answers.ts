084fb12a3e369af0f04b9e2e77aa37989bd6a8ef
HugoNCastro <hugonaaman@gmail.com>
2023-10-30 11:33:49 -0300

    Create use case to list question answers

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,26 @@
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface FetchQuestionsAnswersUseCaseRequest {
+  page: number
+  questionId: string
+}
+
+interface FetchQuestionsAnswersUseCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionsAnswersUseCase {
+  constructor(private answersRepository: AnswersRepository) { }
+
+  async execute({
+    page,
+    questionId
+  }: FetchQuestionsAnswersUseCaseRequest): Promise<FetchQuestionsAnswersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(questionId, { page })
+
+    return {
+      answers,
+    }
+  }
+}
