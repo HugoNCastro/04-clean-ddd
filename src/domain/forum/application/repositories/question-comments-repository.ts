c1f5e5e1a6c85b041aaaf2bec9036dbd8bbc41d5
HugoNCastro <hugonaaman@gmail.com>
2023-10-31 16:57:04 -0300

    Create use case to comment answer

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,5 +1,3 @@
-import { PaginationParams } from '@/core/repositories/pagination-params'
-import { Question } from '../../enterprise/entities/question'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 
 export interface QuestionCommentsRepository {
