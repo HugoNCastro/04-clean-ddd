3204af7e91ca8ffb570d4fd177639c111bba7d2e
HugoNCastro <hugonaaman@gmail.com>
2023-10-31 17:38:32 -0300

    Create use case to list question comments

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,18 +1,23 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
 export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
   public items: QuestionComment[] = []
 
-  async delete(questionComment: QuestionComment){
-    const itemIndex = this.items.findIndex((item) => item.id === questionComment.id)
 
-    this.items.splice(itemIndex, 1)
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
+
   async findById(id: string) {
     const questionComment = this.items.find((item) => item.id.toString() === id)
 
-    if(!questionComment) {
+    if (!questionComment) {
       return null
     }
 
@@ -23,5 +28,10 @@ export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepos
     this.items.push(questionComment)
   }
 
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === questionComment.id)
+
+    this.items.splice(itemIndex, 1)
+  }
 }
 
