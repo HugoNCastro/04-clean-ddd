c27732f09840a86c453d85e0f927bdcff598cd9a
HugoNCastro <hugonaaman@gmail.com>
2023-11-13 17:10:25 -0300

    Create question with attachments

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,10 +3,12 @@ import { Slug } from './value-objects/slug'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
+import { QuestionAttachment } from './question-attachment'
 
 export interface QuestionProps {
   authorId: UniqueEntityID
   bestAnswerId?: UniqueEntityID
+  attachments: QuestionAttachment[]
   title: string
   content: string
   slug: Slug
@@ -47,13 +49,21 @@ export class Question extends AggregateRoot<QuestionProps> {
     return this.props.content
   }
 
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+  get attachments(){
+    return this.props.attachments
   }
 
   get title() {
     return this.props.title
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
   }
 
   set title(title: string) {
@@ -63,17 +73,17 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.touch()
   }
 
-  get bestAnswerId() {
-    return this.props.bestAnswerId
-  }
-
   set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
     this.props.bestAnswerId = bestAnswerId
     this.touch()
   }
 
+  set attachments(attachments: QuestionAttachment[]){
+    this.props.attachments = attachments
+  }
+
   static create(
-    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    props: Optional<QuestionProps, 'createdAt' | 'slug' | 'attachments'>,
     id?: UniqueEntityID,
   ) {
     const question = new Question(
@@ -81,6 +91,7 @@ export class Question extends AggregateRoot<QuestionProps> {
         ...props,
         slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: props.createdAt ?? new Date(),
+        attachments: props.attachments ?? []
       },
       id,
     )
